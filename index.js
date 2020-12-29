40cebc811641c71174af349405bb28bf37f8e82c
vaytlou <36851964+vaytlou@users.noreply.github.com>
2020-12-29 16:08:48 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+require('http').Server((req, res) => {
+  const author = 'itmo282167'
+  res.writeHead(200, {
+    'X-Author': author,
+    'Content-Type': 'text/plain; charset=UTF-8',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers',
+  })
+
+  if (req.url === '/login/') {
+    return res.end(author)
+  }
+
+  if (req.url === '/sample/') {
+    function task(x) {
+      const sqrt = this ** 2
+      return x * sqrt
+    }
+
+    return res.end(String(task))
+  }
+
+  res.end(author)
+}).listen(process.env.PORT)
